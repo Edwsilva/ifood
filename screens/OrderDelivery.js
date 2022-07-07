cf04057c19585b17a4ed9a6817edb9f669819c2e
Edwilson Silva <26224374@rio.rj.gov.br>
2022-07-07 11:28:57 -0300

    first commit

---
+++ screens/OrderDelivery.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity
+} from "react-native";
+//import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+//import MapViewDirections from "react-native-maps-directions";
+
+import { COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY } from "../constants"
+
+const OrderDelivery = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>Order Delivery</Text>
+    </View>
+  )
+}
+
+export default OrderDelivery;
---
+++ screens/Restaurant.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    Animated
+} from "react-native";
+//import { isIphoneX } from 'react-native-iphone-x-helper'
+
+import { icons, COLORS, SIZES, FONTS } from '../constants'
+
+const Restaurant = () => {
+    return (
+       <View>
+        <Text>Restaurant</Text>
+       </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray2
+    }
+})
+
+export default Restaurant;
