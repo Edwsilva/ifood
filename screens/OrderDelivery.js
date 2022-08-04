5393fd7504fafc005e83a0291aecae33826a7a70
Edwilson <edwsilva@gmail.com>
2022-08-04 09:50:18 -0300

    Ajuste do androidManifest.xml
    e build.gradle seguindo o link https://stackoverflow.com/a/58914729
    atenção para não criar outro chave defautConfig

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -5,9 +5,18 @@ import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 //import MapViewDirections from 'react-native-maps-directions';
 
-import {COLORS, FONTS, icons, SIZES, GOOGLE_API_KEY} from '../constants';
+import {COLORS, FONTS, icons, SIZES} from '../constants';
 
 const OrderDelivery = () => {
+  // function renderMap() {
+  //   return (
+  //     <View style={{...StyleSheet.absoluteFillObject}}>
+  //       <MapView style={{flex: 1}} />
+  //     </View>
+  //   );
+  // }
+
+  // return <View style={{flex: 1}}>{renderMap()}</View>;
   return (
     <MapView
       // provider={PROVIDER_GOOGLE} // remove if not using Google Maps
@@ -18,16 +27,16 @@ const OrderDelivery = () => {
         latitudeDelta: 0.015,
         longitudeDelta: 0.0121,
       }}></MapView>
-
-    // <MapView
-    //   initialRegion={{
-    //     latitude: 37.78825,
-    //     longitude: -122.4324,
-    //     latitudeDelta: 0.0922,
-    //     longitudeDelta: 0.0421,
-    //   }}
-    // />
   );
+  // <MapView
+  //   initialRegion={{
+  //     latitude: 37.78825,
+  //     longitude: -122.4324,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   }}
+  // />
+  //);
 
   //  import React, {useState, useRef, useEffect} from 'react';
   // import {Animated, ScrollView, View} from 'react-native';
@@ -82,5 +91,18 @@ const OrderDelivery = () => {
   //     </>
   //   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default OrderDelivery;
